6a987bb710808fe84f0f26a78d8fa183775ee29e
Difei Xiao <21656761@student.uwa.edu.au>
2024-08-12 15:07:23 +0800

    will not close the console window after running the program
    Signed-off-by: Difei Xiao <21656761@student.uwa.edu.au>

--- HotWeir_Calibation.py
+++ HotWeir_Calibation.py
@@ -3,62 +3,68 @@ import numpy as np
 import matplotlib.pyplot as plt
 from HotWeir_Util import HotWeir_Util
 
+try:
+    # Load the YAML configuration file
+    with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
 
-# Load the YAML configuration file
-with open('config.yaml', 'r') as file:
-    config = yaml.safe_load(file)
-
-# Get Real Wind Speed from Config file
-Corresponding_windSpeed = config['Polyfit_Parameter']['Corresponding_windSpeed']
-WindSpeed_relationship = config['Polyfit_Parameter']['WindSpeed_relationship']
-# Get the two coefficients in the "WindSpeed_relationship".
-relationship_a, relationship_b = HotWeir_Util.extract_numbers_from_string(WindSpeed_relationship)
-real_windSpeed_Hz = HotWeir_Util.extract_numbers_from_string(Corresponding_windSpeed)
-real_windSpeed = []
-for windSpeed in real_windSpeed_Hz:
-    if windSpeed == 0.0:
-        # When motor is 0hz, set the wind speed to 0m/s
-        real_windSpeed.append(0.0)
-    else:
-        real_windSpeed.append(relationship_a * windSpeed + relationship_b)
-
-
-# Get the mean electric signal for each file.
-file_numbers = HotWeir_Util.extract_range_from_string(config['Files_number']['Calibration_file'])
-signal_mean = []
-for file_number in file_numbers:
-    electric_value = HotWeir_Util.get_lists_file(file_number)
-    signal_mean.append(np.mean(electric_value))
+    # Get Real Wind Speed from Config file
+    Corresponding_windSpeed = config['Polyfit_Parameter']['Corresponding_windSpeed']
+    WindSpeed_relationship = config['Polyfit_Parameter']['WindSpeed_relationship']
+    # Get the two coefficients in the "WindSpeed_relationship".
+    relationship_a, relationship_b = HotWeir_Util.extract_numbers_from_string(WindSpeed_relationship)
+    real_windSpeed_Hz = HotWeir_Util.extract_numbers_from_string(Corresponding_windSpeed)
+    real_windSpeed = []
+    for windSpeed in real_windSpeed_Hz:
+        if windSpeed == 0.0:
+            # When motor is 0hz, set the wind speed to 0m/s
+            real_windSpeed.append(0.0)
+        else:
+            real_windSpeed.append(relationship_a * windSpeed + relationship_b)
 
 
-# PolyFit the electric signal to real wind speed.
-Polyfit_degree = int(config['Polyfit_Parameter']['Polyfit_degree'])
-x = np.array(signal_mean)
-y = np.array(real_windSpeed)
-coefficients = np.polyfit(x, y, Polyfit_degree)
+    # Get the mean electric signal for each file.
+    file_numbers = HotWeir_Util.extract_range_from_string(config['Files_number']['Calibration_file'])
+    signal_mean = []
+    for file_number in file_numbers:
+        electric_value = HotWeir_Util.get_lists_file(file_number)
+        signal_mean.append(np.mean(electric_value))
 
 
-# Write the coefficients into "Calibration_result.yaml" and set isPolyfit to "True"
-coefficients_list = coefficients.tolist()
-# Structure for the new YAML file
-calibration_result = {
-    'Calibration_Result': {
-        'Coefficients': coefficients_list,
-        'isPolyfit': True,
+    # PolyFit the electric signal to real wind speed.
+    Polyfit_degree = int(config['Polyfit_Parameter']['Polyfit_degree'])
+    x = np.array(signal_mean)
+    y = np.array(real_windSpeed)
+    coefficients = np.polyfit(x, y, Polyfit_degree)
+
+
+    # Write the coefficients into "Calibration_result.yaml" and set isPolyfit to "True"
+    coefficients_list = coefficients.tolist()
+    # Structure for the new YAML file
+    calibration_result = {
+        'Calibration_Result': {
+            'Coefficients': coefficients_list,
+            'isPolyfit': True,
+        }
     }
-}
-# Write the new configuration to the YAML file
-with open('Calibration_result.yaml', 'w') as file:
-    yaml.safe_dump(calibration_result, file, default_flow_style=False)
+    # Write the new configuration to the YAML file
+    with open('Calibration_result.yaml', 'w') as file:
+        yaml.safe_dump(calibration_result, file, default_flow_style=False)
 
-print("Calibration result saved successfully")
+    print("Calibration result saved successfully")
 
 
-# Plot the raw data and polyfit function
-polynomial = np.poly1d(coefficients)
-y_fit = polynomial(x)
+    # Plot the raw data and polyfit function
+    polynomial = np.poly1d(coefficients)
+    y_fit = polynomial(x)
 
-plt.scatter(x, y, color='red', label='Points')
-plt.plot(x, y_fit, label='Curves')
-plt.legend()
-plt.show()
+    plt.scatter(x, y, color='red', label='Points')
+    plt.plot(x, y_fit, label='Curves')
+    plt.legend()
+    plt.show()
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+finally:
+    input("Press Enter to exit...")
--- HotWeir_MeanRMS.py
+++ HotWeir_MeanRMS.py
@@ -3,36 +3,43 @@ import numpy as np
 from HotWeir_Util import HotWeir_Util
 import yaml
 
-# Load the "config.yaml" for WindSpeed file name
-with open('config.yaml', 'r') as file:
-    config = yaml.safe_load(file)
-    WindSpeed_files = HotWeir_Util.extract_range_from_string(config['Files_number']['WindSpeed_file'])
+try:
+    # Load the "config.yaml" for WindSpeed file name
+    with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+        WindSpeed_files = HotWeir_Util.extract_range_from_string(config['Files_number']['WindSpeed_file'])
 
-# Load the "Calibration_result.yaml" for calibration coefficient.
-with open('Calibration_result.yaml', 'r') as file:
-    config = yaml.safe_load(file)
-    if not config['Calibration_Result']['isPolyfit']:
-        raise ValueError("Need to Run the calibration program first!")
-    Coefficients = config['Calibration_Result']['Coefficients']
+    # Load the "Calibration_result.yaml" for calibration coefficient.
+    with open('Calibration_result.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+        if not config['Calibration_Result']['isPolyfit']:
+            raise ValueError("Need to Run the calibration program first!")
+        Coefficients = config['Calibration_Result']['Coefficients']
 
-Coefficients.reverse()
-mean_value = []
-RMS_value = []
-for WindSpeed_file in WindSpeed_files:
-    eSignals = HotWeir_Util.get_lists_file(WindSpeed_file)
-    windSpeeds = []
-    for eSignal in eSignals:
-        windSpeed = 0.0
-        for i in range(len(Coefficients)):
-            windSpeed = windSpeed + Coefficients[i] * (eSignal ** i)
-        windSpeeds.append(windSpeed)
-    mean_value.append(np.mean(windSpeeds))
-    RMS_value.append(np.std(windSpeeds))
+    Coefficients.reverse()
+    mean_value = []
+    RMS_value = []
+    for WindSpeed_file in WindSpeed_files:
+        eSignals = HotWeir_Util.get_lists_file(WindSpeed_file)
+        windSpeeds = []
+        for eSignal in eSignals:
+            windSpeed = 0.0
+            for i in range(len(Coefficients)):
+                windSpeed = windSpeed + Coefficients[i] * (eSignal ** i)
+            windSpeeds.append(windSpeed)
+        mean_value.append(np.mean(windSpeeds))
+        RMS_value.append(np.std(windSpeeds))
 
-# Write the result into excel.
-headers = ['file name', 'mean value', 'RMS value']
-data = []
-for fls, mv, rms in zip(WindSpeed_files, mean_value, RMS_value):
-    data.append([fls, mv, rms])
+    # Write the result into excel.
+    headers = ['file name', 'mean value', 'RMS value']
+    data = []
+    for fls, mv, rms in zip(WindSpeed_files, mean_value, RMS_value):
+        data.append([fls, mv, rms])
 
-HotWeir_Util.write_to_excel(headers,data,'output.xlsx')
+    HotWeir_Util.write_to_excel(headers,data,'output.xlsx')
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+finally:
+    input("Press Enter to exit...")
